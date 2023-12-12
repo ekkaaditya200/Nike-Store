c14aeec22336fecd4de44387bdb4d26ace366ba0
Aditya Ekka <adityaekka2003@gmail.com>
2023-12-12 22:29:28 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import {FlexContent, Hero, Sales } from "./Components/index"
-import { heroapi, popularsales, toprateslaes } from "./Data/data"
+import { FlexContent, Hero, Sales, Stories, Footer, Navbar, Cart } from "./Components/index"
+import { heroapi, popularsales, toprateslaes, highlight, sneaker, story, footerAPI } from "./Data/data"
 function App() {
   return (
     <>
+      <Navbar />
+      <Cart/>
       <main className="flex flex-col gap-16 relative">
         <Hero heroapi={heroapi}></Hero>
-        {/* Conditional statement */}
-        <Sales endpoint={popularsales} ifExists/> 
+        <Sales endpoint={popularsales} ifExists />
+        <FlexContent endpoint={highlight} ifExists></FlexContent>
         <Sales endpoint={toprateslaes} />
-        <FlexContent></FlexContent>
+        <FlexContent endpoint={sneaker}></FlexContent>
+        <Stories story={story}></Stories>
       </main>
+      <Footer footerAPI={footerAPI}></Footer>
     </>
   )
 }
